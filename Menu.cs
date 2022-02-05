79d3a405d7fc939180e05d041d20e8294d590d2a
dwood1030 <dalton.woodard1@gmail.com>
2022-02-05 12:19:31 -0500

    created main menu class and method, started working on customers class and new customer method

---
+++ Menu.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStoreApp
+{
+    internal class Menu
+    {
+        public static bool MainMenu()
+        {
+            //  offer options to user
+            //   - CREATE NEW CUSTOMER
+            Console.WriteLine("1. CREATE NEW CUSTOMER");
+            //   - CUSTOMER SEARCH
+            Console.WriteLine("2. CUSTOMER SEARCH");
+            //   - CHECK OUT/RETURN MOVIE
+            Console.WriteLine("3. CHECKOUT/RETURN");
+            //   - EDIT STOCK
+            Console.WriteLine("4. EDIT INVENTORY");
+            //  ATTEMPT ERROR HANDLING (user does something i am not expecting)
+            Console.WriteLine("5. EXIT");
+
+            Console.Write("ENTER YOUR CHOICE: ");
+            // ask for their choice
+            string userInput = Console.ReadLine();
+
+            switch(userInput)
+            {
+                case "1":
+                    Console.WriteLine();
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine();
+                    Customers.NewCustomer();
+                    break;
+
+                case "2":
+                    Console.WriteLine();
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine();
+                    break;
+
+                case "3":
+                    Console.WriteLine();
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine();
+                    break;
+
+                case "4":
+                    Console.WriteLine();
+                    Console.WriteLine("============================================================");
+                    Console.WriteLine();
+                    break;
+
+                case "5":
+                    return false;
+
+                default:
+                    Console.WriteLine($"{userInput} IS INVALID");
+                    Console.WriteLine("TRY AGAIN");
+                    break;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MovieStoreApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============================================================");
+            Console.WriteLine("MOVIES-R-US -- MAIN MENU");
+            Console.WriteLine("============================================================");
+
+            bool exit = true;
+            while (exit)
+            {
+                exit = Menu.MainMenu();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
